018b35344580ad3a9545aade27bcdc103cecb065
Nata <94069522+tatah32@users.noreply.github.com>
2022-03-03 22:10:24 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -32,7 +32,7 @@ global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.owner = ["6289668014332"] //ganti agar fitur owner bisa di gunakan
 global.packname = '\n\n' //sticker wm ubah
-global.author = ${@user} //sticker wm ganti nama kalian
+global.author = ${@user} '${@user}' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
